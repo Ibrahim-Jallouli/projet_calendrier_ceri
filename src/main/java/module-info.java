56dd85ad31344520e0100fd808036356884dce7e
Ibrahim-Jallouli <ibrahim.jallouli@alumni.univ-avignon.fr>
2024-02-13 11:21:01 +0100

    project structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.example.m1prototypage {
     requires javafx.fxml;
 
 
-    opens com.example.M1IHM to javafx.fxml;
-    exports com.example.M1IHM;
+    opens com.example.m1prototypage to javafx.fxml;
+    exports com.example.m1prototypage;
+    exports com.example.m1prototypage.controller;
+    opens com.example.m1prototypage.controller to javafx.fxml;
 }
